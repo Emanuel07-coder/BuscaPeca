e64f08c142792bdb2fa4d868a4622ddbc4466e9c
Emanuel Campos <emanuel.wenzel2007@gmail.com>
2026-05-27 21:08:30 -0400

    Update supabase.ts

--- frontend/src/utils/supabase.ts
+++ frontend/src/utils/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Pegamos a URL e a Key das variáveis de ambiente do Vite
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
+// Inicializamos o cliente do Supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);
